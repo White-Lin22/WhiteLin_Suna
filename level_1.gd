f46c6b46bcd765d6fba76e971d8b7348e02dd5e7
White-Lin22 <white.lin@education.nsw.gov.au>
2025-07-02 14:40:38 +1000

    level 1 started

---
+++ level_1.gd
@@ -0,0 +1,8 @@
+extends Node3D
+
+
+@export var target : CharacterBody3D
+# calls the enemy which is in its own group, sets the target position as the player's position
+func _process(delta: float) -> void:
+	get_tree().call_group("enemy", "target_position", target.global_transform.origin)
+	
